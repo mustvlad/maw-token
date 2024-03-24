a181a5523e4b2727aa96ad744338213af7d62efb
Vlad Mustiata <vlad@deqod.com>
2024-03-24 16:38:58 +0200

    tests: cleanup

--- test/maw/MAW.ts
+++ test/maw/MAW.ts
@@ -1,4 +1,3 @@
-// import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
@@ -43,53 +42,19 @@ describe("MAW", function () {
       this.otherAccount = otherAccount;
     });
 
-    //   describe("Validations", function () {
-    //     it("Should revert with the right error if called too soon", async function () {
-    //       await expect(this.lock.withdraw()).to.be.revertedWithCustomError(this.lock, "UnlockTimeNotReached");
-    //     });
-
-    //     it("Should revert with the right error if called from another account", async function () {
-    //       // We can increase the time in Hardhat Network
-    //       await time.increaseTo(this.unlockTime);
-
-    //       // We use lock.connect() to send a transaction from another account
-    //       await expect(this.lock.connect(this.otherAccount).withdraw()).to.be.revertedWithCustomError(
-    //         this.lock,
-    //         "NotOwner",
-    //       );
-    //     });
-
-    //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-    //       // Transactions are sent using the first signer by default
-    //       await time.increaseTo(this.unlockTime);
-
-    //       await expect(this.lock.withdraw()).not.to.be.reverted;
-    //     });
-    //   });
-
-    //   describe("Events", function () {
-    //     it("Should emit an event on withdrawals", async function () {
-    //       await time.increaseTo(this.unlockTime);
-
-    //       await expect(this.lock.withdraw()).to.emit(this.lock, "Withdrawal").withArgs(this.lockedAmount, anyValue); // We accept any value as `when` arg
-    //     });
-    //   });
-
     describe("Transfers", function () {
-      // it("Should transfer the funds to the owner", async function () {
-      //   await time.increaseTo(this.unlockTime);
-
-      //   await expect(this.lock.withdraw()).to.changeEtherBalances(
-      //     [this.owner, this.lock],
-      //     [this.lockedAmount, -this.lockedAmount],
-      //   );
-      // });
-
       it("Should be able to transfer tokens to another account", async function () {
         const amount = ethers.parseUnits("100", 18);
         await this.maw.transfer(this.otherAccount.address, amount);
 
         expect(await this.maw.balanceOf(this.otherAccount.address)).to.equal(amount);
+      });
+
+      it("Should be able to burn tokens", async function () {
+        const amount = ethers.parseUnits("100", 18);
+        await this.maw.burn(amount);
+
+        expect(await this.maw.totalSupply()).to.equal(ethers.parseUnits("999999900", 18));
       });
     });
   });
